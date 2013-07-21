504503e6e5a232625e5333d0e8652423f8e11f38
John Smith <example@stanford.edu>
2013-07-21 18:01:42 +0000

    Added web.js with read

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2');
 });
+
+var data = fs.readFileSync("index.html", "utf8");
+console.log(data);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
